d87be151abbebd960e3634348c1ce86da5a2654d
demystik <demystiktech@gmail.com>
2023-05-10 18:16:34 +0100

    deffering procedures

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/pup
-# Using puppet to install flask(2.1.0) from pip3
-
-package { 'flask' :
-  ensure   => '2.1.0',
-  provider => 'pip3'
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,6 +0,0 @@
-# Using Puppet, create a manifest that kills a process named killmenow
-
-exec { 'kill process':
-  command  => 'pkill killmenow',
-  provider => 'shell',
-}
