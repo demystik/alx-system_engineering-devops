f0d3091c5a4ea6b7cb1b43465be967e9f3b40e41
demystik <demystiktech@gmail.com>
2023-08-27 04:24:20 +0100

    1-user_limit.pp

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# Enable user holberton to login and open file error free.
+
+# Elevate hard file threshold 4 Holberton usr.
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# To elevate soft file limit4 Holberton usr.
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
